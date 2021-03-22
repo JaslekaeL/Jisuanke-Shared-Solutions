5e77d0636554ce5077ee3dc25042230d235768e8
JaslekaeL <65748856+JaslekaeL@users.noreply.github.com>
2021-03-22 21:30:26 +0800

    Create T1003.cpp

---
+++ Question Solutions/T1003.cpp
@@ -0,0 +1,13 @@
+//T1003：按照样例输出字符组成的菱形图案。
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    char a;
+    scanf("%c", &a);
+    printf("  %c\n", a);
+    printf(" %c%c%c\n", a, a, a);
+    printf("%c%c%c%c%c\n", a, a, a, a, a);
+    printf(" %c%c%c\n", a, a, a);
+    printf("  %c\n", a);
+    return 0;
+}
