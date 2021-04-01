5c6c03a7450c2addba79cc7e688a02e304ce2b59
JaslekaeL <65748856+JaslekaeL@users.noreply.github.com>
2021-04-01 22:48:36 +0800

    Create T1052.cpp

---
+++ Question Solutions/T1052.cpp
@@ -0,0 +1,16 @@
+//T1051：输入一组数据,计算其均值。
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+int main(){
+    int n, i;
+    double a, s;
+    cin >> n;
+    s = 0;
+    for(i = 1; i <= n; i++){
+        cin >> a;
+        s = s+a;
+    }
+    printf("%0.4f", s/n);
+}
